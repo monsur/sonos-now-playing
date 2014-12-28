e5726919bbc1f3ff02f3dd8e90d61f9a6fe6c817
Monsur Hossain <monsur@gmail.com>
2014-12-28 15:35:24 -0600

    Fuck IE

--- src/static/index.js
+++ src/static/index.js
@@ -1,24 +1,6 @@
 var currentTrack = null;
 var previousTracks = [];
 
-var createCORSRequest = function(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // Check if the XMLHttpRequest object has a "withCredentials" property.
-    // "withCredentials" only exists on XMLHTTPRequest2 objects.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // Otherwise, check if XDomainRequest.
-    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // Otherwise, CORS is not supported by the browser.
-    xhr = null;
-  }
-  return xhr;
-}
-
 var AlbumArt = function() {
   this.cache = {};
 };
@@ -57,7 +39,8 @@ AlbumArt.prototype.get = function(artist, album, callback) {
   }
 
   var url = this.createUrl(artist, album);
-  var xhr = createCORSRequest('GET', url);
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url, true);
 
   xhr.onerror = function() {
     console.log('There was an error!');
