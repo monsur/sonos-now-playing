6fc9406cfa1faa1aab36ab6a281779aa435662ec
Monsur Hossain <monsur@gmail.com>
2014-12-29 15:08:51 -0600

    Adding task to copy files to dest dir

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,10 +3,19 @@ module.exports = function(grunt) {
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
       all: ['Gruntfile.js', 'src/**/*.js']
+    },
+    copy: {
+      main: {
+        files: [
+          {expand: true, cwd: 'src/server/', src: ['*.js'], dest: 'dest/', flatten: true},
+          {expand: true, cwd: 'src/client/', src: ['**'], dest: 'dest/static'}
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint', 'copy']);
 };
