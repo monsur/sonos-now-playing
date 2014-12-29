740a1feefa5ab3045e23de7c271e320799b46e9c
Monsur Hossain <monsur@gmail.com>
2014-12-29 15:52:38 -0600

    Adding rule to concat js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,21 +2,42 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      all: ['Gruntfile.js', 'src/**/*.js']
+      beforeconcat: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        '!src/client/js/intro.js',
+        '!src/client/js/outro.js'
+      ],
+      afterconcat: ['dest/static/js/index.js']
+    },
+    concat: {
+      dist: {
+        src: [
+          'src/client/js/intro.js',
+          'src/client/js/last-fm-album-art.js',
+          'src/client/js/album-art-cache.js',
+          'src/client/js/screensaver.js',
+          'src/client/js/ui-controller.js',
+          'src/client/js/main.js',
+          'src/client/js/outro.js'
+        ],
+        dest: 'dest/static/js/index.js',
+      },
     },
     copy: {
       main: {
         files: [
           {expand: true, cwd: 'src/', src: ['config.json'], dest: 'dest/', flatten: true},
           {expand: true, cwd: 'src/server/', src: ['*.js'], dest: 'dest/', flatten: true},
-          {expand: true, cwd: 'src/client/', src: ['**'], dest: 'dest/static'}
+          {expand: true, cwd: 'src/client/', src: ['**', '!**/js/**'], dest: 'dest/static'}
         ]
       }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.registerTask('default', ['jshint', 'copy']);
+  grunt.registerTask('default', ['jshint:beforeconcat', 'concat', 'jshint:afterconcat', 'copy']);
 };
