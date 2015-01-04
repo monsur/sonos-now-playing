1199727f474005771b8d8f4c3fbcfc51ed3db00d
Monsur Hossain <monsur@gmail.com>
2015-01-04 15:31:25 -0600

    Adding new test for error handler.

--- tests/server/sonos-controller.test.js
+++ tests/server/sonos-controller.test.js
@@ -1,3 +1,4 @@
+var assert = require('assert');
 var SonosController = require('../../src/server/sonos-controller');
 
 describe('subscribe', function() {
@@ -10,5 +11,23 @@ describe('subscribe', function() {
       return;
     }
     throw new Error('Expected an error.');
+  });
+
+  it('Verifies the correct callback header', function() {
+    var s = new SonosController();
+    s.makeRequest = function(options, callback) {
+      assert.equal('<foo>', options.headers['CALLBACK']);
+    };
+    s.subscribe('foo');
+  });
+
+  it('Processes an error', function() {
+    var s = new SonosController();
+    s.makeRequest = function(options, callback) {
+      callback({msg: 'ERROR'}, null);
+    };
+    s.subscribe('foo', function(error, res) {
+      assert.equal('ERROR', error.msg);
+    });
   });
 });
