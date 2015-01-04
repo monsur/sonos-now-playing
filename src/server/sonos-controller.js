afa2f09e212f9042592fe3fb3785bad3356f7c4e
Monsur Hossain <monsur@gmail.com>
2015-01-04 14:47:15 -0600

    Adding subscribe method to SonosController

--- src/server/sonos-controller.js
+++ src/server/sonos-controller.js
@@ -1,11 +1,44 @@
+var http = require('http');
 
 var port = 1400;
 
-var SonosController = function(speakerIp) {
-  this.speakerIp = speakerIp;
-}
+var parseTimeout = function(val) {
+  var prefix = 'Second-';
+  return parseInt(val.substr(prefix.length));
+};
 
-SonosController.prototype.subscribe = function(callback) {
+var SonosController = function(speakerIp, logger) {
+  this.speakerIp = speakerIp;
+  this.logger = logger;
+};
+
+SonosController.prototype.subscribe = function(callbackUrl, callback) {
+  if (!callbackUrl) {
+    throw new Error('Must specify a callback URL.');
+  }
+  var options = {};
+  options.method = 'SUBSCRIBE';
+  options.path = '/MediaRenderer/AVTransport/Event';
+  options.headers = {
+    'CALLBACK': '<' + callbackUrl + '>',
+    'NT': 'upnp:event'
+  };
+  this.makeRequest(options, function(error, res) {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    var headers = res.headers;
+    var data = {};
+    if ('sid' in headers) {
+      data.sid = headers.sid;
+    }
+    if ('timeout' in headers) {
+      data.timeout = parseTimeout(headers.timeout);
+    }
+    callback(null, data);
+  });
 };
 
 SonosController.prototype.renew = function(callback) {
@@ -21,6 +54,11 @@ SonosController.prototype.pause = function(callback) {
 };
 
 SonosController.prototype.next = function(callback) {
+};
+
+SonosController.prototype.makeRequest = function(options, callback) {
+  options.hostname = this.speakerIp;
+  options.port = port;
 };
 
 module.exports = SonosController;
