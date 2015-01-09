a46e3d59aa0b6c4133c0f4c1a75a91f25366ad9b
Monsur Hossain <monsur@gmail.com>
2015-01-08 19:01:03 -0600

    Adding actions for play/pause/next, removing the word 'controller'

--- src/action-controller.js
+++ src/action-controller.js
@@ -1,7 +1,7 @@
 var Logger = require('little-logger').Logger;
 var http = require('http');
 
-var ActionController = function(speakerIp, port, action, body, response, logger) {
+var Action = function(speakerIp, port, action, body, response, logger) {
   this.request = {
     method: 'POST',
     hostname: speakerIp,
@@ -16,13 +16,13 @@ var ActionController = function(speakerIp, port, action, body, response, logger)
   this.response = response;
 };
 
-ActionController.prototype.createBody = function(body) {
+Action.prototype.createBody = function(body) {
   return '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" ' +
     's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body>' +
     body + '</s:Body></s:Envelope>';
 };
 
-ActionController.prototype.send = function(callback) {
+Action.prototype.send = function(callback) {
   var that = this;
   var req = http.request(request, function(res) {
     var error = getError(res);
@@ -38,4 +38,44 @@ ActionController.prototype.send = function(callback) {
   req.end();
 };
 
-module.exports = ActionController;
+
+var PlayAction = function(speakerIp, port, logger) {
+  this.action = new Action(speakerIp, port,
+      'action',
+      'body',
+      'response',
+      logger);
+};
+PlayAction.prototype.send = function(callback) {
+  this.action.send(callback);
+};
+
+
+var PauseAction = function(speakerIp, port, logger) {
+  this.action = new Action(speakerIp, port,
+      'action',
+      'body',
+      'response',
+      logger);
+};
+PauseAction.prototype.send = function(callback) {
+  this.action.send(callback);
+};
+
+
+var NextAction = function(speakerIp, port, logger) {
+  this.action = new Action(speakerIp, port,
+      'action',
+      'body',
+      'response',
+      logger);
+};
+NextAction.prototype.send = function(callback) {
+  this.action.send(callback);
+};
+
+module.exports = {
+  'PlayAction': PlayAction,
+  'PauseAction': PauseAction,
+  'NextAction': NextAction
+};
