9af6745431992c1705d4a3a22a4a0f82f64bf6c6
Monsur Hossain <monsur@gmail.com>
2015-01-09 00:08:15 -0600

    simplifying action controller

--- src/action-controller.js
+++ src/action-controller.js
@@ -1,30 +1,61 @@
 var Logger = require('little-logger').Logger;
 var http = require('http');
 
-var Action = function(speakerIp, port, action, body, response, logger) {
-  this.request = {
+var PLAY = 'Play',
+    PAUSE = 'Pause',
+    Next = 'Next';
+
+var createBody = function(action) {
+  return '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" ' +
+    's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body>' +
+    '<u:' + action + ' xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">' +
+    '<InstanceID>0</InstanceID><Speed>1</Speed></u:' + action + '>' +
+    '</s:Body></s:Envelope>';
+};
+
+var createResponse = function(action) {
+  return '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" ' +
+    's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body>' +
+    '<u:' + action + 'Response ' +
+    'xmlns:u="urn:schemas-upnp-org:service:AVTransport:1">' +
+    '</u:' + action + 'Response></s:Body></s:Envelope>';
+};
+
+
+var ActionController = function(speakerIp, port, logger) {
+  this.speakerIp = speakerIp;
+  this.port = port;
+
+  var requests = {};
+  requests[PLAY] = this.createRequest(PLAY);
+  requests[PAUSE] = this.createRequest(PAUSE);
+  requests[NEXT] = this.createRequest(NEXT);
+  this.requests = requests;
+};
+
+ActionController.prototype.createRequest = function(action) {
+  var request = {
     method: 'POST',
-    hostname: speakerIp,
-    port: port,
+    hostname: this.speakerIp,
+    port: this.port,
     path: '/MediaRenderer/AVTransport/Control',
     headers: {
       'Content-Type': 'text/xml',
-      'SOAPACTION': action
+      'SOAPACTION': '"urn:schemas-upnp-org:service:AVTransport:1#' + action + '"'
     }
   };
-  this.body = this.createBody(body);
-  this.response = response;
+  this.body = createBody(action);
+  this.response = createResponse(action);
+  return {
+    request: request,
+    body: body,
+    response: response
+  };
 };
 
-Action.prototype.createBody = function(body) {
-  return '<s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" ' +
-    's:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/"><s:Body>' +
-    body + '</s:Body></s:Envelope>';
-};
-
-Action.prototype.send = function(callback) {
+ActionController.prototype.send = function(data, callback) {
   var that = this;
-  var req = http.request(request, function(res) {
+  var req = http.request(data.request, function(res) {
     var error = getError(res);
     if (error) {
       return callback(error, null);
@@ -34,48 +65,21 @@ Action.prototype.send = function(callback) {
   req.on('error', function(e) {
     that.logger.error(e.message);
   });
-  req.write(this.body);
+  req.write(data.body);
   req.end();
 };
 
-
-var PlayAction = function(speakerIp, port, logger) {
-  this.action = new Action(speakerIp, port,
-      'action',
-      'body',
-      'response',
-      logger);
-};
-PlayAction.prototype.send = function(callback) {
-  this.action.send(callback);
+ActionController.prototype.play = function(callback) {
+  this.send(this.requests[PLAY], callback);
 };
 
-
-var PauseAction = function(speakerIp, port, logger) {
-  this.action = new Action(speakerIp, port,
-      'action',
-      'body',
-      'response',
-      logger);
-};
-PauseAction.prototype.send = function(callback) {
-  this.action.send(callback);
+ActionController.prototype.pause = function(callback) {
+  this.send(this.requests[PAUSE], callback);
 };
 
-
-var NextAction = function(speakerIp, port, logger) {
-  this.action = new Action(speakerIp, port,
-      'action',
-      'body',
-      'response',
-      logger);
-};
-NextAction.prototype.send = function(callback) {
-  this.action.send(callback);
+ActionController.prototype.next = function(callback) {
+  this.send(this.requests[NEXT], callback);
 };
 
-module.exports = {
-  'PlayAction': PlayAction,
-  'PauseAction': PauseAction,
-  'NextAction': NextAction
-};
+module.exports = ActionController;
+
