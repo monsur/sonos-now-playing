d0b56830cdbf0ddb77f4771683d4b6b912d354d9
Monsur Hossain <monsur@gmail.com>
2015-02-01 14:14:00 -0600

    Change to nodemon

--- bin/run.sh
+++ bin/run.sh
@@ -1,3 +1,3 @@
-/opt/node/bin/node /home/pi/Documents/sonos-now-playing/dest/app.js &
-sleep 10
+/opt/node/bin/nodemon /home/pi/Documents/sonos-now-playing/dest/app.js &
+sleep 30
 /usr/bin/chromium --kiosk --incognito http://localhost:8080/index.html &
