47bb76fa008e72440eabddc78a16af89b3c3b098
Monsur Hossain <monsur@gmail.com>
2015-02-01 15:17:29 -0600

    Fixing run script

--- bin/run.sh
+++ bin/run.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-/opt/node/bin/nodemon /home/pi/Documents/sonos-now-playing/dest/app.js &
-response = 0
-while [ "$response" -ne 200 ]
+nodemon dest/app.js &
+response=0
+while [ $response -ne 200 ]
 do
   sleep 2
-  response=$(curl --write-out %{http_code} --silent --output /dev/null http://10.0.1.17:8080/health)
+  response=$(curl --write-out %{http_code} --silent --output /dev/null http://localhost:8080/health)
+  echo $response
 done
 /usr/bin/chromium --kiosk --incognito http://localhost:8080/index.html &
