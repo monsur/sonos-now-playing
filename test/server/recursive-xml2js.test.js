9731f5d3b2891037f096ef81f0982c29864dcb7b
Monsur Hossain <monsur@gmail.com>
2015-04-08 00:56:04 -0500

    Adding unit tests for isXmlString

--- test/server/recursive-xml2js.test.js
+++ test/server/recursive-xml2js.test.js
@@ -1,2 +1,17 @@
 var assert = require('assert');
 var RecursiveXml2Js = require('../../src/server/recursive-xml2js');
+
+describe('parse recursive xml', function() {
+
+  var parser = new RecursiveXml2Js();
+
+  it('is xml string', function() {
+    assert.equal(parser.isXmlString(), false);
+    assert.equal(parser.isXmlString(null), false);
+    assert.equal(parser.isXmlString(123), false);
+    assert.equal(parser.isXmlString(''), false);
+    assert.equal(parser.isXmlString('a'), false);
+    assert.equal(parser.isXmlString('><'), false);
+    assert.equal(parser.isXmlString('<'), true);
+  });
+});
