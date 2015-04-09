f1d722a6c4595e0df0a673be48a50d55bc0b2b35
Monsur Hossain <monsur@gmail.com>
2015-04-09 00:27:48 -0500

    refactoring tests

--- test/server/recursive-xml2js.test.js
+++ test/server/recursive-xml2js.test.js
@@ -1,11 +1,18 @@
 var assert = require('assert');
 var RecursiveXml2Js = require('../../src/server/recursive-xml2js');
 
+var testParseXml = function(input, expected) {
+  var parser = new RecursiveXml2Js();
+  parser.parse(input, function(error, result) {
+    assert.deepEqual(result, expected);
+  });
+};
+
 describe('parse recursive xml', function() {
 
-  var parser = new RecursiveXml2Js();
 
   it('is xml string', function() {
+    var parser = new RecursiveXml2Js();
     assert.equal(parser.isXmlString(), false);
     assert.equal(parser.isXmlString(null), false);
     assert.equal(parser.isXmlString(123), false);
@@ -16,58 +23,40 @@ describe('parse recursive xml', function() {
   });
 
   it('empty string', function() {
-    parser.parse('', function(error, result) {
-      assert.deepEqual(result, {});
-    });
+    testParseXml('', {});
   });
 
   it('string that is not xml', function() {
-    parser.parse('abcde', function(error, result) {
-      assert.deepEqual(result, {});
-    });
+    testParseXml('abcde', {});
   });
 
   it('empty xml node', function() {
-    parser.parse('<a></a>', function(error, result) {
-      assert.deepEqual(result, {'a': ''});
-    });
+    testParseXml('<a></a>', {'a': ''});
   });
 
   it('empty xml node (shorthand)', function() {
-    parser.parse('<a />', function(error, result) {
-      assert.deepEqual(result, {'a': ''});
-    });
+    testParseXml('<a />', {'a': ''});
   });
 
   it('xml node with value', function() {
-    parser.parse('<a>123</a>', function(err, result) {
-      assert.deepEqual(result, {'a': '123'});
-    });
+    testParseXml('<a>123</a>', {'a': '123'});
   });
 
   it('xml node with attribute', function() {
-    parser.parse('<a attr="foo"></a>', function(err, result) {
-      assert.deepEqual(result, {'a': {'attr': 'foo'}});
-    });
+    testParseXml('<a attr="foo"></a>', {'a': {'attr': 'foo'}});
   });
 
   it('xml node with attribute and value', function() {
-    parser.parse('<a attr="foo">123</a>', function(err, result) {
-      assert.deepEqual(result, {'a': {'_': 123, 'attr': 'foo'}});
-    });
+    testParseXml('<a attr="foo">123</a>', {'a': {'_': 123, 'attr': 'foo'}});
   });
 
   it('nested xml in attribute', function() {
-    parser.parse('<a attr="&lt;b&gt;123&lt;/b&gt;"></a>',
-      function(err, result) {
-        assert.deepEqual(result, {"a":{"attr":{"b":"123"}}});
-      });
+    testParseXml('<a attr="&lt;b&gt;123&lt;/b&gt;"></a>',
+      {"a":{"attr":{"b":"123"}}});
   });
 
   it('nested xml in value', function() {
-    parser.parse('<a>&lt;b&gt;123&lt;/b&gt;</a>',
-      function(err, result) {
-        assert.deepEqual(result, {"a":{"b":"123"}});
-      });
+    testParseXml('<a>&lt;b&gt;123&lt;/b&gt;</a>',
+        {"a":{"b":"123"}});
   });
 });
