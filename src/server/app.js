60bdef14d6423881e8c3e5ad9f2d7bf053962698
Monsur Hossain <monsur@gmail.com>
2015-04-11 00:04:06 -0500

    Migrating to new methods

--- src/server/app.js
+++ src/server/app.js
@@ -3,8 +3,13 @@ var http = require('http'),
   logger = require('little-logger'),
   socketio = require('socket.io'),
   config = require('./config'),
-  SonosController = require('./sonos-controller'),
-  SonosEvent = require('./event');
+  ActionController = require('./action-controller'),
+  SonosEvent = require('./event'),
+  RecursiveXml2Js = require('./recursive-xml2js');
+
+var getCallbackUrl = function(ip, port, callbackPath) {
+  return 'http://' + ip + ':' + port + callbackPath;
+};
 
 // The number of connected clients.
 var connections = 0;
@@ -15,18 +20,42 @@ var logger = new logger.Logger(options.loglevel, {
     format: '   %l  - %a'
 });
 
-var sonos = new SonosController(options, logger);
-
-var statusEvent = // TODO
-
-var notificationHandler = new NotificationHandler(logger, function(data) {
-  io.sockets.emit('newTrack', data);
+var statusEvent = new SonosEvent({
+  speakerIp: options.speakerIp,
+  port: options.speakerPort,
+  path: '/MediaRenderer/AVTransport/Event',
+  callbackUrl: getCallbackUrl(options.ip, options.port, options.callbackPath),
+  handler: function(data) {
+    io.sockets.emit('newTrack', data);
+  }
 });
+
+var actions = new ActionController(options.speakerIp, options.speakerPort, logger);
 
 var app = express();
 app.use(express.static(__dirname + '/static'));
 app.notify(options.callbackPath, function(req, res, next) {
-  notificationHandler.handle(req, res, next);
+
+  var body = '';
+  req.on('data', function(chunk) {
+    body += chunk.toString();
+  });
+  req.on('end', function() {
+    // Respond to the request first so we don't keep Sonos waiting.
+    res.writeHead(200);
+    res.end();
+
+    var parser = new RecursiveXml2Js();
+    parser.parse(body, function(err, result) {
+      if (err) {
+        throw new Error(err);
+      }
+      var opts = {};
+      //events.handle(opts);
+      console.log(JSON.stringify(result, null, 2));
+    });
+  });
+
 });
 app.get('/js/config.js', config.getHandler(options));
 app.get('/refresh', function(req, res, next) {
@@ -45,21 +74,26 @@ io.sockets.on('connection', function(socket) {
 
   connections++;
   if (connections === 1) {
-    sonos.subscribe();
+    statusEvent.subscribe();
   }
 
   socket.on('play', function(data) {
-    sonos.togglePlay(data.state);
+    console.log(data);
+    if (data.state) {
+      actions.play();
+    } else {
+      actions.pause();
+    }
   });
 
   socket.on('next', function(data) {
-    sonos.next();
+    actions.next();
   });
 
   socket.on('disconnect', function() {
     connections--;
     if (connections === 0) {
-      sonos.unsubscribe();
+      statusEvent.unsubscribe();
     }
   });
 });
