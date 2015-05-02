217897d72c7c4d244c855e713c2bf69123263fed
Monsur Hossain <monsur@gmail.com>
2015-05-02 17:13:15 -0500

    Remove speaker name from command line

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /home/pi/Documents/sonos-now-playing/dest
-/opt/node/bin/node ./app.js `/opt/node/bin/node ./findCoordinator.js --name "Dining Room"` &
+/opt/node/bin/node ./app.js `/opt/node/bin/node ./findCoordinator.js` &
 response=0
 while [ $response -ne 200 ]
 do
