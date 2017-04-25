f2282e55835b6dba7c8180c18327ff26c060dd02
Monsur Hossain <monsur@gmail.com>
2017-04-24 23:18:09 -0500

    Cache the next image

--- src/client/js/photos-controller.js
+++ src/client/js/photos-controller.js
@@ -1,10 +1,14 @@
 var PhotosController = function() {
   this.timeoutId = null;
+  this.cache = [];
+  this.cachePos = 0;
+  this.showPos = 0;
   this.elem = document.getElementById('imageContent');
 };
 
 PhotosController.prototype.start = function() {
   this.getPhoto();
+  this.showPhoto();
 };
 
 PhotosController.prototype.stop = function() {
@@ -13,15 +17,22 @@ PhotosController.prototype.stop = function() {
   }
 };
 
-PhotosController.prototype.getPhoto = function() {
+PhotosController.prototype.showPhoto = function() {
   var that = this;
   this.elem.className = 'fadeOut';
   setTimeout(function() {
     that.elem.src = '';
-    that.elem.src = 'http://localhost:8080/photo?' + Math.floor(Math.random() * 1000000);
+    that.elem.src = that.cache[that.showPos++ % 2].src;
     that.elem.className = "";
+    that.getPhoto();
     that.timeoutId = setTimeout(function() {
-        that.getPhoto();
+        that.showPhoto();
       }, 5000);
-    }, 800);
+    }, 600);
+};
+
+PhotosController.prototype.getPhoto = function() {
+  var img = new Image();
+  img.src = 'http://localhost:8080/photo?' + Math.floor(Math.random() * 1000000);
+  this.cache[this.cachePos++ % 2] = img;
 };
