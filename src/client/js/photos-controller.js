9a6912ebf22f746c733a56328454e668249ba442
Monsur Hossain <monsur@gmail.com>
2017-04-26 22:41:37 -0500

    adding smoother image loading

--- src/client/js/photos-controller.js
+++ src/client/js/photos-controller.js
@@ -1,14 +1,31 @@
 var PhotosController = function() {
+  var that = this;
   this.timeoutId = null;
-  this.pos = 0;
+
+  var handler = function(event) {
+    that.transitionendHandler(event);
+  };
+
   this.elem = [];
-  this.elem[0] = document.getElementById('slideshow0');
-  this.elem[1] = document.getElementById('slideshow1');
+  for (var i = 0; i < 2; i++) {
+    var img = document.createElement('img');
+    img.id = 'slideshow' + i;
+    img.className = i === 0 ? 'off' : 'on';
+    img.addEventListener('transitionend', handler, false);
+    this.elem.push(img);
+    document.getElementById('slideshow').appendChild(img);
+  }
 };
 
 PhotosController.prototype.start = function() {
-  this.getPhoto();
-  this.showPhoto();
+  for (var i = 0; i < this.elem.length; i++) {
+    var img = this.elem[i];
+    img.className = img.className == 'on' ? 'off' : 'on';
+  }
+  var that = this;
+  this.timeoutId = setTimeout(function() {
+    that.start();
+  }, 6000);
 };
 
 PhotosController.prototype.stop = function() {
@@ -17,23 +34,12 @@ PhotosController.prototype.stop = function() {
   }
 };
 
-PhotosController.prototype.showPhoto = function() {
-  if (this.elem[0].className == 'on') {
-    this.elem[0].className = 'off';
-    this.elem[1].className = 'on';
-  } else {
-    this.elem[0].className = 'on';
-    this.elem[1].className = 'off';
+PhotosController.prototype.transitionendHandler = function(event) {
+  if (event.target.className == 'off') {
+    event.target.src = getPhotoUrl();
   }
-  var that = this;
-  setTimeout(function() {
-    that.getPhoto();
-    that.timeoutId = setTimeout(function() {
-      that.showPhoto();
-    }, 6000);
-  }, 1100);
 };
 
-PhotosController.prototype.getPhoto = function() {
-  this.elem[this.pos++ % 2].src = 'http://localhost:8080/photo?' + Math.floor(Math.random() * 1000000);
+var getPhotoUrl = function() {
+  return 'http://localhost:8080/photo?' + Math.floor(Math.random() * 1000000);
 };
