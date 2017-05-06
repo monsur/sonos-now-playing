70fab098c621636917b711477451b53c42980600
Monsur Hossain <monsur@gmail.com>
2017-05-05 23:03:33 -0500

    Fixed ssdp by reverting to previous version

--- bin/run.sh
+++ bin/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 /bin/sleep 3
 cd /home/pi/Documents/sonos-now-playing/dest
-node ./app.js --live true --speakerIp 192.168.86.49 &
+node ./app.js --live true --speakerIp `node findCoordinator.js` &
 response=0
 while [ $response -ne 200 ]
 do
