f4e93dad1593a83569aedacdc87d79a435e2b840
Monsur Hossain <monsur@gmail.com>
2017-05-06 00:02:25 -0500

    Calling stop() after all processing has completed

--- src/server/findCoordinator.js
+++ src/server/findCoordinator.js
@@ -16,21 +16,35 @@ var coordinator = new Coordinator(options);
 // Use ssdp to find a single Sonos server. Any Sonos server will do. The rest
 // of the Sonos data will be retrieved from the Sonos speaker itself.
 var client = new ssdp();
+var state = 0;
 
 client.on('response', function(headers, code, rinfo) {
   // Since we only need a single Sonos IP, stop processing after we receive
   // the first one.
-  client._stop();
+  if (state > 0) {
+    return;
+  }
+  state = 1;
 
   // Retrieve the Sonos topology from the one speaker.
   coordinator.fromSpeakerIp(rinfo.address, function(err, c) {
     if (err) {
       console.log(err);
-      return;
+    } else {
+      console.log(c.ip);
     }
-    console.log(c.ip);
+    state = 2;
   });
 });
 
   // Search for Sonos speakers only.
 client.search('urn:schemas-upnp-org:device:ZonePlayer:1');
+
+var stop = function() {
+  if (state === 2) {
+    client.stop();
+  } else {
+    setTimeout(stop, 100);
+  }
+};
+stop();
